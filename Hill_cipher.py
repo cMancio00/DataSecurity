1f7940fc5a4cae3226e632c5c462a5b6c5eeaf72
Christian Mancini <mancinichristian00@gmail.com>
2023-03-30 16:32:35 +0200

    Hill Cipher goes BRRRRRRRRRRRRR

---
+++ Hill_cipher.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sympy import Matrix
+import logging
+from string_preprocessing import prepare_message, number_to_string
+
+logging.basicConfig(format='%(asctime)s - %(message)s\n', filename='hill_cipher.log', filemode='w', level=logging.INFO, datefmt='%d-%b-%y %H:%M:%S')
+
+def crypt(plaintext:np.ndarray, key:np.ndarray,block_size)->np.ndarray:
+    plaintext.resize((int(len(plaintext)/block_size) , block_size))
+    ciphertext = np.dot(plaintext, key) % 26
+    logging.info("[CIPHERTEXT]\n{ciphertext}\n{text}".format(ciphertext = ciphertext, text = number_to_string(ciphertext)))
+    return ciphertext
+
+def decrypt(ciphertext:np.ndarray, key:np.ndarray)->np.ndarray:
+    key_inv = Matrix(key).inv_mod(26)
+    key_inv = np.array(key_inv).astype(float)
+    logging.info("[KEY INVERSE]\n{key_inv}".format(key_inv = key_inv))
+    plaintext = np.dot(ciphertext,key_inv) % 26
+    logging.info("[PLAINTEXT]\n{plaintext}\n{text}".format(plaintext = plaintext, text = number_to_string(plaintext)))
+    return plaintext
+
+def generate_key(block_size:int)->np.ndarray:
+    while True:
+        key = np.random.randint(0, 26, size=(block_size, block_size))
+        try:
+            Matrix(key).inv_mod(26)
+        except ValueError:
+            continue
+        logging.info("[KEY GENERATED]\n{key}".format(key=key))
+        return key
+
+def main():
+    message = "Il cifrario di Hill appartiene ai cifrari monoalfabetici a blocchi"
+    block_size = np.random.randint(2, 10)
+    key = generate_key(block_size)
+    message = prepare_message(message, block_size)
+    crypted = crypt(message, key, block_size)
+    decrypted = decrypt(crypted, key)
+    print("Message was: {message}".format(message = number_to_string(decrypted)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ string_preprocessing.py
@@ -0,0 +1,31 @@
+import numpy as np
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s\n', filename='hill_cipher.log', filemode='w', level=logging.INFO, datefmt='%d-%b-%y %H:%M:%S')
+
+def add_padding(message:str, block_size:int)->str:
+    if len(message) % block_size != 0:
+        message += "X" * (block_size - (len(message) % block_size))
+    return message
+
+def sring_to_number(message:str)-> np.ndarray:
+    message = message.upper()
+    return np.asanyarray(list(message.encode('ascii'))) - 65
+
+def number_to_string(message:np.ndarray)->str:
+    message = np.ravel(message)
+    message = list(message)
+    return "".join([chr(int(letter)+65) for letter in message])
+
+def remove_spaces(message:str)->str:
+    return message.replace(" ", "")
+
+def prepare_message(message:str, block_size:int)->str:
+    message = message.upper()
+    logging.info("[MESSAGE TO CRYPT]\n{message}".format(message = message))
+    message = remove_spaces(message)
+    message = add_padding(message, block_size)
+    logging.info("[MESSAGE PROCESSED]\n{message}".format(message = message))
+    message = sring_to_number(message)
+    logging.info("[MESSAGE TO NUMBERS]\n{message}".format(message = message))
+    return message
