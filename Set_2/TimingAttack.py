062e916a6e27529ce2370982e516f4c471150ef3
Christian Mancini <mancinichristian00@gmail.com>
2023-05-17 18:06:55 +0200

    Refactored Timing attack

--- Set_2/TimingAttack.py
+++ Set_2/TimingAttack.py
@@ -3,33 +3,33 @@ import numpy as np
 
 def main():
     ta = TimingAttack()
-    d = [1]
-    for i in range(1,64):
-          
-        variance = generateObservations(d,ta)
+    exponent = [1]
+    for _ in range(1,64):
+        variance = generateObservations(exponent,ta)
         if variance[0] < variance[1]:
-            d.append(0)
+            exponent.append(0)
         else:
-            d.append(1)
-    print(d)
-    ta.test(d)
+            exponent.append(1)
+    print(exponent)
+    ta.test(exponent)
 
-def generateObservations(d:list[int],ta:TimingAttack)->np.array:
+def generateObservations(exponent:list[int],ta:TimingAttack)->list[int]:
     observations0 = []
     observations1 = []
-    
     for _ in range(2000):
-        c = np.random.randint(0,(2**62-1))
-        realTime = ta.victimdevice(c)
-        d.append(0)
-        observations0.append(realTime - ta.attackerdevice(c,d))
-        del d[-1]
-        d.append(1)
-        observations1.append(realTime - ta.attackerdevice(c,d))
-        del d[-1]
+        chipertext = np.random.randint(0,(2**62-1))
+        realTime = ta.victimdevice(chipertext)
+        observations0.append(trybit(0,ta,exponent,realTime,chipertext))
+        observations1.append(trybit(1,ta,exponent,realTime,chipertext))
     var0 = np.array(observations0)
     var1 = np.array(observations1)
     return [np.var(var0),np.var(var1)]
+
+def trybit(bit:int,ta:TimingAttack,exponent:int,realTime:int,chipertext:int)->int:
+    exponent.append(bit)
+    observation = realTime - ta.attackerdevice(chipertext,exponent)
+    del exponent[-1]
+    return observation
 
     
 if __name__ == "__main__":
