dbc83fe70079925596f2bcb0aa97479f0f66ee48
Christian Mancini <mancinichristian00@gmail.com>
2023-05-17 16:13:41 +0200

    Timing attack works

--- Set_2/TimingAttack.py
+++ Set_2/TimingAttack.py
@@ -4,29 +4,32 @@ import numpy as np
 def main():
     ta = TimingAttack()
     d = [1]
-    for i in range(1,63):
-        c = np.random.randint(0,(2**61 - 1))    
-        d.append(1)
-        observations = generateObservations(d,ta,c)
-        var1 = np.var(observations)
-        del d[-1]
-        d.append(0)
-        observations = generateObservations(d,ta,c)
-        var0 = np.var(observations)
-        del d[-1]
-        if var1 < var0:
-            d.append(1)
-        else:
+    for i in range(1,64):
+          
+        variance = generateObservations(d,ta)
+        if variance[0] < variance[1]:
             d.append(0)
+        else:
+            d.append(1)
     print(d)
     ta.test(d)
 
-def generateObservations(d:list[int],ta:TimingAttack,c:int)->np.array:
-    observations = []
-    realTime = ta.victimdevice(c)
-    for _ in range(100):
-        observations.append(realTime - ta.attackerdevice(c,d))
-    return np.array(observations)
+def generateObservations(d:list[int],ta:TimingAttack)->np.array:
+    observations0 = []
+    observations1 = []
+    
+    for _ in range(2000):
+        c = np.random.randint(0,(2**62-1))
+        realTime = ta.victimdevice(c)
+        d.append(0)
+        observations0.append(realTime - ta.attackerdevice(c,d))
+        del d[-1]
+        d.append(1)
+        observations1.append(realTime - ta.attackerdevice(c,d))
+        del d[-1]
+    var0 = np.array(observations0)
+    var1 = np.array(observations1)
+    return [np.var(var0),np.var(var1)]
 
     
 if __name__ == "__main__":
